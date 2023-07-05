13c79782e2c113c259ebf27784f72e0336807b4d
Thomas Faddegon <tomfaddegon@gmail.com>
2023-07-05 14:16:08 -0400

    fixed responsive nav issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import Planet from "./components/Planet";
 
 function App() {
   return (
-    <div className="">
+    <div className="flex flex-col">
       <Nav />
-      <div className="flex-column flex min-h-[100vh] w-full justify-center pt-24">
+      <div className="flex-col flex min-h-[100vh] w-full justify-center pt-24">
         <Planet />
       </div>
     </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -39,15 +39,15 @@ const Nav: React.FC = () => {
     "flex flex-row border-b border-slate-800 py-6 uppercase w-full items-center justify-start";
 
   return (
-    <nav>
-      <div className="flex w-full h-[6rem] flex-row justify-center border-b-[1px] border-slate-800">
-        <div className="flex w-full h-full justify-between md:w-[95rem] md:flex-col lg:flex-row px-4">
-          <h2 className="mb-0 text-3xl md:mb-12 lg:mb-0 self-center">
+    <nav className="flex flex-col md:flex-row">
+      <div className="flex w-full h-full xl:h-[6rem] flex-row justify-center border-b-[1px] border-slate-800">
+        <div className="flex w-full h-full items-center justify-between md:max-w-[95rem] md:flex-col xl:flex-row py-6 xl:py-0 px-4">
+          <h2 className="mb-0 text-3xl md:mb-8 xl:mb-0 self-center">
             THE PLANETS
           </h2>
 
           {/* desktop / tablet nav */}
-          <ul className="hidden h-fullflex-row gap-8 text-sm font-medium uppercase tracking-wider text-veryLightGrey md:flex">
+          <ul className="hidden h-[3rem] flex-row gap-5 xl:gap-8 text-sm font-medium uppercase tracking-wider text-veryLightGrey md:flex">
             {planets.map((planet) => {
               const activeStyle =
                 planet.toLowerCase() === currentPlanet.toLowerCase()
@@ -85,43 +85,15 @@ const Nav: React.FC = () => {
 
       {/* mobile nav */}
       {isNavOpen && (
-        <div className="flex-column flex items-center justify-center">
+        <div className="flex-col flex items-center justify-center">
           <ul className="mx-8 flex min-h-[250px] w-full flex-col items-center justify-between font-medium tracking-widest text-veryLightGrey">
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/mercury">Mercury</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/earth">Earth</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/mars">Mars</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/jupiter">Jupiter</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/saturn">Saturn</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/uranus">Uranus</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
-            <li className={listItemStyles}>
-              <span className="mr-5">O</span>
-              <a href="/neptune">Neptune</a>
-              <div className="ml-auto mr-4">A</div>
-            </li>
+            {planets.map((planet) => (
+              <li key={planet} className={listItemStyles}>
+                <span className="mr-5">O</span>
+                {planet}
+                <div className="ml-auto mr-4">A</div>
+              </li>
+            ))}
           </ul>
         </div>
       )}
