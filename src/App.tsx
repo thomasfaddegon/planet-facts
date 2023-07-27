b7f0cf08fa25b0acfb39bb0424b12a3f32e3fc8d
Thomas Faddegon <tomfaddegon@gmail.com>
2023-07-27 19:53:28 -0400

    added stars background

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import "./App.css";
 
 import Nav from "./components/Nav";
 import Planet from "./components/Planet";
+import backgroundStars from "../public/images/background-stars.svg";
 
 function App() {
   return (
-    <div className="flex flex-col mb-8">
-      <Nav />
-      <div className="flex-col flex w-full justify-center items-center pt-12 lg:pt-8 mb-32 md:mb-0">
-        <Planet />
+    <div
+      className="flex flex-col mb-8 h-[100vh]"
+      style={{ backgroundImage: `url(${backgroundStars})` }}
+    >
+      <div className="flex flex-col mb-8 ">
+        <Nav />
+        <div className="flex-col flex w-full justify-center items-center pt-12 lg:pt-8 mb-32 md:mb-0">
+          <Planet />
+        </div>
       </div>
     </div>
   );
