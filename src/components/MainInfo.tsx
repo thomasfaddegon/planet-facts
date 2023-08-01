4d22ca57d6e4ca7b1e276aa31c4fad8ac3a05c15
Thomas Faddegon <tomfaddegon@gmail.com>
2023-08-01 10:20:13 -0400

    fixed source font issue

--- src/components/MainInfo.tsx
+++ src/components/MainInfo.tsx
@@ -18,14 +18,14 @@ const MainInfo: React.FC = () => {
         {planetData?.[section].content}
       </p>
 
-      <div className="flex flex-row items-center justify-center md:justify-start gap-1 hover:underline hover:text-white">
+      <div className="flex flex-row items-center justify-center md:justify-start gap-1 hover:underline">
         <a
           href={planetData?.[section].source}
           target="_blank"
           className="flex flex-row gap-1 items-center"
         >
-          <span>Source: Wikipedia</span>
-          <img src={arrow} className="w-3 h-3" />
+          <p>Source: Wikipedia</p>
+          <img src={arrow} className="w-3 h-3 translate-y-[-1px]" />
           {""}
         </a>
       </div>
